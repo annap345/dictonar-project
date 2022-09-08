74bb9226616fe79668e6b7159093698d864700ee
annap345 <popovych.anna345@gmail.com>
2022-09-08 22:15:42 +0200

    added link to github

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header>Dictonary</header>
+      <footer>✨<a href="https://github.com/annap345"> coded by Anna Popovch</a>✨</footer>
       <Dictonary/>
     </div>
   );
